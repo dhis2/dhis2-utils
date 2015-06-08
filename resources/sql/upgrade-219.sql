8d0f2ad70520ee17caa34a2e1465be1980b40d83
Lars Helge Overland <larshelge@gmail.com>
2015-06-08 10:00:06 +0200

    Upgrade

--- resources/sql/upgrade-219.sql
+++ resources/sql/upgrade-219.sql
@@ -8,11 +8,3 @@ set organisationunitid = (
   select organisationunitid from trackedentityinstance tei
   where pi.trackedentityinstanceid = tei.trackedentityinstanceid)
 where pi.organisationunitid is null;
-
--- Upgrade program data entry form foreign keys
-
-alter table programstage drop constraint fk_programstage_dataentryform;
-alter table programstage add constraint fk_programstage_dataentryform foreign key (dataentryform) references dataentryform (dataentryformid) match SIMPLE on update no action on delete no action;
-
-alter table trackedentityform drop constraint fk_trackedentityform_dataentryformid;
-alter table trackedentityform add constraint fk_trackedentityform_dataentryformid foreign key (dataentryform) references dataentryform (dataentryformid) match SIMPLE on update no action on delete no action;
