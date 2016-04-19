2740f283119e781a1571a3b2bb3b08a1b5d5f76e
Lars Helge Overland <larshelge@gmail.com>
2016-04-19 18:33:47 +0200

    Minor

--- tools/dhis-adhoc/src/main/java/org/hisp/dhis/adhoc/command/RandomDataUsagePopulator.java
+++ tools/dhis-adhoc/src/main/java/org/hisp/dhis/adhoc/command/RandomDataUsagePopulator.java
@@ -23,26 +23,31 @@ public class RandomDataUsagePopulator
         DateTime end = new DateTime( 2016, 12, 31, 0, 0 );
         
         int w = 0;
+        int c = 0;
         
         while ( start.isBefore( end ) )
-        {
-            if ( w % 3 == 0 )
-            {
-                w++;
-            }
-                        
-            DataStatistics stats = new DataStatistics( rani( 110, 170 ), 
+        {                        
+            DataStatistics stats = new DataStatistics( 
+                ( rani( 110, 130 ) + w ),
+                
                 ( rand( 410, 610 ) + w ), ( rand( 510, 710 ) + w ), ( rand( 410, 610 ) + w ), ( rand( 470, 630 ) + w ), 
                 ( rand( 520, 610 ) + w ), ( rand( 620, 910 ) + w ), ( rand( 3200, 3400 ) + w ), 
                 
                 ( rand( 110, 150 ) + w ), ( rand( 120, 160 ) + w ), ( rand( 110, 150 ) + w ), ( rand( 130, 160 ) + w ), 
                 ( rand( 110, 160 ) + w ), ( rand( 140, 180 ) + w ), ( rand( 140, 190 ) + w ),
                 
-                ( rani( 110, 150 ) + w ) );
+                ( rani( 140, 160 ) + w ) );
             
             stats.setCreated( start.toDate() );
             
             dataStatisticsService.saveDataStatistics( stats );
+            
+            c++;
+            
+            if ( c % 5 == 0 )
+            {
+                w++;
+            }
             
             start = start.plusDays( 1 );
         }
@@ -61,5 +66,4 @@ public class RandomDataUsagePopulator
         r += min;
         return r;
     }
-
 }
