9bbaa935ae2700f4e3ad779840da093ecf844534
Lars Helge Overland <larshelge@gmail.com>
2016-04-21 08:47:46 +0200

    Mino

--- tools/dhis-adhoc/src/main/java/org/hisp/dhis/adhoc/command/RandomDataUsagePopulator.java
+++ tools/dhis-adhoc/src/main/java/org/hisp/dhis/adhoc/command/RandomDataUsagePopulator.java
@@ -28,15 +28,13 @@ public class RandomDataUsagePopulator
         while ( start.isBefore( end ) )
         {                        
             DataStatistics stats = new DataStatistics( 
-                ( rani( 110, 130 ) + w ),
-                
                 ( rand( 410, 610 ) + w ), ( rand( 510, 710 ) + w ), ( rand( 410, 610 ) + w ), ( rand( 470, 630 ) + w ), 
                 ( rand( 520, 610 ) + w ), ( rand( 620, 910 ) + w ), ( rand( 3200, 3400 ) + w ), 
                 
                 ( rand( 110, 150 ) + w ), ( rand( 120, 160 ) + w ), ( rand( 110, 150 ) + w ), ( rand( 130, 160 ) + w ), 
                 ( rand( 110, 160 ) + w ), ( rand( 140, 180 ) + w ), ( rand( 140, 190 ) + w ),
                 
-                ( rani( 140, 160 ) + w ) );
+                ( rani( 110, 130 ) + w ), ( rani( 140, 160 ) + w ) );
             
             stats.setCreated( start.toDate() );
             
