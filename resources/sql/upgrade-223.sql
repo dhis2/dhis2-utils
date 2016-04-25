2de8fe96eae9cada6f0f506c9b79b4c3ba2ea48d
Lars Helge Øverland <larshelge@gmail.com>
2016-04-25 15:31:38 +0200

    Update upgrade-223.sql

--- resources/sql/upgrade-223.sql
+++ resources/sql/upgrade-223.sql
@@ -3,7 +3,8 @@
 
 update programrulevariable set sourcetype = 'DATAELEMENT_CURRENT_EVENT', lastupdated = current_timestamp 
 where uid in (
-  select prv.uid from programrulevariable prv join program p on p.programid = prv.programid 
+  select prv.uid from programrulevariable prv 
+  inner join program p on p.programid = prv.programid 
   where p.type = 'WITHOUT_REGISTRATION' and (
   prv.sourcetype = 'DATAELEMENT_NEWEST_EVENT_PROGRAM' or prv.sourcetype = 'DATAELEMENT_NEWEST_EVENT_PROGRAM_STAGE')
 );
