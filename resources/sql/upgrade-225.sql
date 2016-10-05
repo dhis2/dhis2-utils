7e081fcfe887cd89b32a7bf99db202fc6ade6eb0
Lars Helge Overland <larshelge@gmail.com>
2016-10-05 19:48:06 +0200

    Minor

--- resources/sql/upgrade-225.sql
+++ resources/sql/upgrade-225.sql
@@ -1,20 +1,25 @@
+
+-- 2.25 upgrade script
+
 -- Add deleted column to datavalue table
+
+-- This will be handled by DHIS 2. Running this script will be much faster in comparison for large datavalue tables.
 
 begin;
 
--- 1) drop indexes and foreign keys
+-- 1) Drop indexes and foreign keys
 
 alter table datavalue drop constraint datavalue_pkey;
 drop index in_datavalue_lastupdated;
 
--- 2) add column, set to false, set to not-null and create index
+-- 2) Add deleted column, set to false, set to not-null and create index
 
 alter table datavalue add column deleted boolean;
 update datavalue set deleted = false where deleted is null;
 alter table datavalue alter column deleted set not null;
 create index in_datavalue_deleted on datavalue(deleted);
 
--- 3) recreate indexes and foreign keys
+-- 3) Recreate indexes and foreign keys
 
 alter table datavalue add constraint datavalue_pkey primary key(dataelementid, periodid, sourceid, categoryoptioncomboid, attributeoptioncomboid);
 create index in_datavalue_lastupdated on datavalue(lastupdated);
