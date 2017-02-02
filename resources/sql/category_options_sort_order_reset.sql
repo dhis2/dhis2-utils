d392423f41abc3236215d914e89d1d03363eb3f7
Lars Helge Øverland <lars@dhis2.org>
2017-02-02 12:12:32 +0100

    Cat opt sort order reset script

---
+++ resources/sql/category_options_sort_order_reset.sql
@@ -0,0 +1,17 @@
+
+-- Drop primary key, create sequence
+
+alter table categories_categoryoptions drop constraint if exists categories_categoryoptions_pkey;
+drop sequence if exists sortorder;
+create sequence sortorder minvalue 1 start 1;
+
+-- Update sort_order for category options
+
+update categories_categoryoptions
+set sort_order = nextval('sortorder')
+where categoryid=492298;
+
+-- Put back primary key, drop sequence
+
+alter table categories_categoryoptions add constraint categories_categoryoptions_pkey primary key(categoryid, sort_order);
+drop sequence sortorder;
