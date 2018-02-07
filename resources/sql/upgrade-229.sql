b247ad006b7f09e8a20f6d8600d18b28d9ea914a
Lars Helge Øverland <lars@dhis2.org>
2018-02-07 14:51:02 +0700

    Data read upgrade 2.29

--- resources/sql/upgrade-229.sql
+++ resources/sql/upgrade-229.sql
@@ -1,3 +1,4 @@
+
 -- 2.29 upgrade script
 
 -- rename tables and columns from "tracked entity" to "tracked entity type"
@@ -18,3 +19,26 @@ alter table trackedentitytypeattributevalues rename trackedentityid to trackeden
 
 alter table trackedentitytranslations rename trackedentityid to trackedentitytypeid;            
 alter table trackedentitytranslations rename constraint fk_objecttranslation_trackedentityid to fk_objecttranslation_trackedentitytypeid;
+
+-- grant data read sharing to users for objects which have metadata read sharing 
+
+update program set publicaccess = 'rwr-----' where publicaccess = 'rw------';
+update program set publicaccess = 'r-r-----' where publicaccess = 'r-------';
+update programstage set publicaccess = 'rwr-----' where publicaccess = 'rw------';
+update programstage set publicaccess = 'r-r-----' where publicaccess = 'r-------';
+update dataset set publicaccess = 'rwr-----' where publicaccess = 'rw------';
+update dataset set publicaccess = 'r-r-----' where publicaccess = 'r-------';
+update dataelementcategoryoption set publicaccess = 'rwr-----' where publicaccess = 'rw------';
+update dataelementcategoryoption set publicaccess = 'r-r-----' where publicaccess = 'r-------';
+
+update usergroupaccess set access = 'rwr-----' where access = 'rw------' and usergroupaccessid in (
+  select usergroupaccessid from programusergroupaccesses
+  union all select usergroupaccessid from programstageuseraccesses
+  union all select usergroupaccessid from datasetuseraccesses
+  union all select usergroupaccessid from dataelementcategoryoption);
+
+update usergroupaccess set access = 'r-r-----' where access = 'r-------' and usergroupaccessid in (
+  select usergroupaccessid from programusergroupaccesses
+  union all select usergroupaccessid from programstageuseraccesses
+  union all select usergroupaccessid from datasetuseraccesses
+  union all select usergroupaccessid from dataelementcategoryoption);
