cb6efe02fd9ab5d9665b5e2c7513c468b1e7b667
Lars Helge Øverland <lars@dhis2.org>
2018-10-10 15:40:25 +0200

    Thread dump

--- resources/util/get_thread_dump.sh
+++ resources/util/get_thread_dump.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-JAVA_PID="$(pidof java)"
+JAVA_PID="$(pidof -s java)"
 JAVA_USER="$(ps -o user= -p ${JAVA_PID})"
 TIMESTAMP=$(date '+%Y%m%d-%H%M%S')
-TARFILE="thread_dump_${TIMESTAMP}.tar.gz"
+TAR_FILE="thread_dump_${TIMESTAMP}.tar.gz"
 OUTPUT_DIR="/tmp"
 DUMP_NO=4
-DUMP_PAUSE=10
+DUMP_PAUSE=1
 
 echo "Java PID: ${JAVA_PID}"
 echo "Java user: ${JAVA_USER}"
 echo "Timestamp: ${TIMESTAMP}"
-echo "Thread dump pause: ${DUMP_PAUSE}s"
+echo "Writing ${DUMP_NO} thread dumps with ${DUMP_PAUSE}s in between"
 echo "---"
 
 cd ${OUTPUT_DIR}
 
-for n in {1..4}
+#for n in {1..4}
+for n in $(seq 1 ${DUMP_NO})
 do
   OUTPUT_FILE="thread_dump_${TIMESTAMP}_${n}.txt"
   sudo -u ${JAVA_USER} jstack -l ${JAVA_PID} > ${OUTPUT_FILE}
@@ -24,7 +25,8 @@ do
   sleep ${DUMP_PAUSE}
 done
 
-tar -czf ${TARFILE} thread_dump_${TIMESTAMP}*.txt
-echo "Wrote compressed tar archive to: ${OUTPUT_DIR}/${TARFILE}"
+tar -czf ${TAR_FILE} thread_dump_${TIMESTAMP}*.txt
+echo "---"
+echo "Wrote compressed tar archive to: ${OUTPUT_DIR}/${TAR_FILE}"
+echo "Done!"
 cd - > /dev/null
-echo "Done"
