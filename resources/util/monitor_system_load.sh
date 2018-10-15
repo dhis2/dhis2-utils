eb0fbc5fcec5554a171c43ee4b1a1acfdae5827a
Lars Helge Øverland <lars@dhis2.org>
2018-10-15 18:39:16 +0200

    System load script

--- resources/util/monitor_system_load.sh
+++ resources/util/monitor_system_load.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Script which creates a thread dump when the average system load is above the given threshold.
+# Depends on `get_thread_dump` to be installed.
+
 if [ $# -eq 0 ]; then
   echo -e "Usage: $0 <threshold>\n"
   exit 1
