6302c58516836e46102590cb62339716920ce9f4
Lars Helge Øverland <lars@dhis2.org>
2018-10-18 10:25:52 +0200

    Monitoring

--- resources/util/monitor_system_load.sh
+++ resources/util/monitor_system_load.sh
@@ -7,9 +7,12 @@ fi
 
 TIMESTAMP=$(date '+%Y%m%d-%H%M%S')
 PRETTY_TIMESTAMP=$(date '+%Y/%m/%d %H:%M:%S')
-OUTPUT_DIR="/tmp"
+OUTPUT_DIR="/var/log/dhis2-monitoring"
+STAT_FILE="${OUTPUT_DIR}/system_stat.log"
 LOAD="$(cat /proc/loadavg | awk '{ print $1 }')"
 THRESHOLD=$1
+CURRENT_USER=$(whoami)
+
 
 #
 # Checks the system load based on 'loadavg' and outputs various system
@@ -18,11 +21,13 @@ THRESHOLD=$1
 function checkLoad() {
   echo "Load average: ${LOAD}"
   echo "Threshold: ${THRESHOLD}"
+  echo "Current user: ${CURRENT_USER}"
   echo ""
 
   if (( $(echo "${LOAD} > ${THRESHOLD}" |bc -l) )); then
     echo "High load detected on system!"
     echo ""
+    createEnvironment
     createSystemStat
     createThreadDump
   else
@@ -31,14 +36,22 @@ function checkLoad() {
 }
 
 #
+# Creates the log directory if it does not already exist.
+#
+function createEnvironment() {
+  sudo mkdir -p ${OUTPUT_DIR}
+  sudo chmod 775 ${OUTPUT_DIR}
+  sudo chown root:${CURRENT_USER} ${OUTPUT_DIR}
+}
+
+#
 # Writes a file with system statistics including free, vmstat, loadavg 
-# and uptime to tmp directory.
+# and uptime to the log directory.
 #
 function createSystemStat() {
-  STAT_FILE="${OUTPUT_DIR}/system_stat_${TIMESTAMP}.log"
-  
-  echo "System statistics at ${PRETTY_TIMESTAMP}" > ${STAT_FILE}
-  echo "---" >> ${STAT_FILE}
+  echo "---------------------------------------" >> ${STAT_FILE}
+  echo "${PRETTY_TIMESTAMP} - System statistics" >> ${STAT_FILE}
+  echo "---------------------------------------" >> ${STAT_FILE}
   echo "" >> ${STAT_FILE}
   
   echo "free " >> ${STAT_FILE}
@@ -60,19 +73,21 @@ function createSystemStat() {
   echo "---" >> ${STAT_FILE}
   uptime  >> ${STAT_FILE}
   echo "" >> ${STAT_FILE}
+  echo "" >> ${STAT_FILE}
   
-  echo "Wrote system stat file to: ${STAT_FILE}"
+  echo "Wrote system stats to: ${STAT_FILE}"
   echo ""
 }
 
 #
 # Writes thread dumps of the Java process to a compressed archive in the
-# tmp directory.
+# log directory.
 #
 function createThreadDump() {
   JAVA_PID="$(pidof -s java)"
   JAVA_USER="$(ps -o user= -p ${JAVA_PID})"
-  TAR_FILE="thread_dump_${TIMESTAMP}.tar.gz"
+  FILE_PREFIX="thread_dump"
+  TAR_FILE="${FILE_PREFIX}_${TIMESTAMP}.tar.gz"
   
   DUMP_NO=3
   DUMP_PAUSE=8
@@ -86,17 +101,20 @@ function createThreadDump() {
 
   for n in $(seq 1 ${DUMP_NO})
   do
-    OUTPUT_FILE="thread_dump_${TIMESTAMP}_${n}.log"
+    OUTPUT_FILE="${FILE_PREFIX}_${TIMESTAMP}_${n}.log"
     sudo -u ${JAVA_USER} jstack -l ${JAVA_PID} > ${OUTPUT_FILE}
     echo "Wrote thread dump to: ${OUTPUT_DIR}/${OUTPUT_FILE}"
     sleep ${DUMP_PAUSE}
   done
 
-  tar -czf ${TAR_FILE} thread_dump_${TIMESTAMP}*.log
+  tar -czf ${TAR_FILE} ${FILE_PREFIX}_${TIMESTAMP}*.log
   echo ""
   echo "Wrote compressed tar archive to: ${OUTPUT_DIR}/${TAR_FILE}"
-  cd - > /dev/null
   echo ""
+  
+  rm ${FILE_PREFIX}_*.log
+  
+  cd - > /dev/null
 }
 
 checkLoad $1
