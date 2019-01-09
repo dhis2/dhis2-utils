9d693e4ea498497aa1dac268ff8deb3afd63704b
Lars Helge Øverland <lars@dhis2.org>
2019-01-09 18:00:37 +0100

    Git log

--- resources/sql/div.sql
+++ resources/sql/div.sql
@@ -613,3 +613,20 @@ where not generate_series in (
 create schema postgis;
 create extension postgis with schema postgis;
 alter database dev set search_path to public, postgis;
+
+
+--Function for creating UIDs
+
+create or replace function generate_uid()  returns text as
+$$
+declare
+chars  text [] := '{0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z}';
+ result text := chars [11 + random() * (array_length(chars, 1) - 11)];
+begin
+ for i in 1..10 loop
+ result := result || chars [1 + random() * (array_length(chars, 1) - 1)];
+ end loop;
+return result;
+end;
+$$
+language plpgsql;
