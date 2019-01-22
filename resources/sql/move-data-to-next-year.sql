4cf89a44819abfaa459fb5905f6a7424955dc6ec
Lars Helge Øverland <lars@dhis2.org>
2019-01-22 11:24:32 +0100

    Moved data SQL

--- resources/sql/move-data-to-next-year.sql
+++ resources/sql/move-data-to-next-year.sql
@@ -3,12 +3,20 @@
 -- Useful for updating demo databases with sample data.
 
 -- (Write) Move startdate and enddate in period to next year
+-- Change the year to reflect the current time. Periods are
+-- moved one year at the time to avoid unique constraint violations.
 
 update period set 
 startdate = (startdate + interval '1 year')::date,
-enddate = (enddate + interval '1 year')::date;
+enddate = (enddate + interval '1 year')::date
+where date_part('year', startdate)::int = 2018;
 
--- (Write) Move programstageinstance and programinstance to next year
+update period set 
+startdate = (startdate + interval '1 year')::date,
+enddate = (enddate + interval '1 year')::date
+where date_part('year', startdate)::int = 2017;
+
+-- (Write) Move programstageinstance
 
 update programstageinstance set 
 duedate = (duedate + interval '1 year'),
@@ -16,6 +24,8 @@ executiondate = (executiondate + interval '1 year'),
 completeddate = (completeddate + interval '1 year'),
 created = (created + interval '1 year'),
 lastupdated = (lastupdated + interval '1 year');
+
+-- (Write) Move programinstance to next year
 
 update programinstance set
 incidentdate = (incidentdate + interval '1 year'),
@@ -27,7 +37,7 @@ lastupdated = (lastupdated + interval '1 year');
 -- (Write) Move interpretations created / lastupdated to next year
 
 update interpretation set created = (created + interval '1 year');
-update interpretation set lastupdated=created;
+update interpretation set lastupdated = created;
 
 -- (Write) Move favorite start/end dates to next year
 
@@ -42,9 +52,9 @@ update eventchart set enddate = (enddate + interval '1 year') where enddate is n
 
 -- Demo db specific statements
 
--- (Write) Move date data element values to next year
+-- (Write) Move date event values to next year
 
-update trackedentitydatavalue set value = to_char((value::date + interval '1 year'), 'YYYY-MM-dd') where dataelementid in (
-  select dataelementid from dataelement where uid in ('eMyVanycQSC','msodh3rEMJa'));
-
-  
+update trackedentitydatavalue set value = to_char((value::date + interval '1 year'), 'YYYY-MM-dd') 
+where dataelementid in (
+  select dataelementid from dataelement where valuetype in ('DATE','DATETIME') and domaintype = 'TRACKER'
+);
