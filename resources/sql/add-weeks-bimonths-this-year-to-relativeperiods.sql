b09147cae1042c5b3b7ead0fe7a10615dea055ed
Lars Helge Øverland <lars@dhis2.org>
2021-01-29 09:13:59 +0100

    Delete add-weeks-bimonths-this-year-to-relativeperiods.sql

--- resources/sql/add-weeks-bimonths-this-year-to-relativeperiods.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE relativeperiods ADD COLUMN weeksthisyear BOOLEAN;
-ALTER TABLE relativeperiods ADD COLUMN bimonthsthisyear BOOLEAN;
-
-UPDATE relativeperiods SET weeksthisyear=FALSE, bimonthsthisyear=FALSE;
