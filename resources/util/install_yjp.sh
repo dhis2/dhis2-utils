7ed07560ff8e625c9601e65fe8f53ceb1b654519
Lars Helge Øverland <lars@dhis2.org>
2021-02-07 18:18:38 +0100

    Add YourKit install script

---
+++ resources/util/install_yjp.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Installs YourKit Java Profiler
+# Run as root with 'sudo ./install_yjp.sh'
+
+YJP_URL="https://s3-eu-west-1.amazonaws.com/content.dhis2.org/development/yjp/yjp.zip" 
+YJP_FILE="yjp.zip"
+INSTALL_DIR="/var/lib"
+AGENT_FILE="${INSTALL_DIR}/yjp/bin/linux-x86-64/libyjpagent.so"
+
+echo "Dowloading YourKit archive.."
+
+rm -f ${YJP_FILE}
+
+wget ${YJP_URL}
+
+if [ -f "${YJP_FILE}" ]; then
+  echo "Downloaded file: ${YJP_FILE}"
+else
+  echo "File not found: ${YJP_URL}"
+  exit 1
+fi
+
+echo "Extracting archive.."
+
+unzip -q ${YJP_FILE} -d /tmp
+
+echo "Installing YourKit.."
+
+chown root:root /tmp/yjp -R
+mv /tmp/yjp ${INSTALL_DIR}
+
+if [ -f "${AGENT_FILE}" ]; then
+  echo "Agent successfully installed at: ${AGENT_FILE}"
+  echo "Add the following to JAVA_OPTS:"
+  echo "-agentpath:${AGENT_FILE}"
+else
+  echo "Installation failed"
+  exit 1
+fi
+
+echo ""
