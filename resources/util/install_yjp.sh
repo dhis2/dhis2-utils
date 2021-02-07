e5acc84fa9eb9471599141adef9d5eadf54a74ec
Lars Helge Øverland <lars@dhis2.org>
2021-02-07 20:10:34 +0100

    Update YJP

--- resources/util/install_yjp.sh
+++ resources/util/install_yjp.sh
@@ -10,6 +10,8 @@ YJP_URL="https://s3-eu-west-1.amazonaws.com/content.dhis2.org/development/yjp/yj
 YJP_FILE="yjp.zip"
 INSTALL_DIR="/var/lib"
 AGENT_FILE="${INSTALL_DIR}/yjp/bin/linux-x86-64/libyjpagent.so"
+TOMCAT_CONF_DIR="/etc/tomcat/conf.d"
+TOMCAT_CONF_FILE="${TOMCAT_CONF_DIR}/yjp.conf"
 
 echo "Dowloading YourKit archive.."
 
@@ -35,12 +37,26 @@ chown root:root /tmp/yjp -R
 mv /tmp/yjp ${INSTALL_DIR}
 
 if [ -f "${AGENT_FILE}" ]; then
-  echo "Agent successfully installed at: ${AGENT_FILE}"
-  echo "Enable agent by adding to JAVA_OPTS:"
-  echo "-agentpath:${AGENT_FILE}"
+  echo "Agent installed: ${AGENT_FILE}"
 else
-  echo "Installation failed"
+  echo "Could not install agent"
   exit 1
 fi
 
+echo "Enabling agent.."
+
+if [ -d "${TOMCAT_CONF_DIR}" ]; then
+  echo "JAVA_OPTS=\"\$JAVA_OPTS -agentpath:${AGENT_FILE}\"" > ${TOMCAT_CONF_FILE}
+
+  if [ -f "${TOMCAT_CONF_FILE}" ]; then
+    echo "Tomcat conf file created: ${TOMCAT_CONF_FILE}"
+  else
+    echo "Tomcat conf file could not be created"
+  fi
+else
+  echo "Could not install Tomcat config, install manuall by adding to JAVA_OPTS:"
+  echo "-agentpath:${AGENT_FILE}"
+fi
+
+echo "Installation done"
 echo ""
