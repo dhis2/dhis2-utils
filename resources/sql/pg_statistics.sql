2259e0362feb6840c03c3f5a87e274e9f0e57bfe
Lars Helge Øverland <lars@dhis2.org>
2021-11-07 22:01:33 +0100

    Update pg_statistics.sql

--- resources/sql/pg_statistics.sql
+++ resources/sql/pg_statistics.sql
@@ -41,13 +41,6 @@ from pg_class
 where relname like 'analytics_%'
 order by approximate_row_count desc;
 
--- Approximate count of rows in raw data and event tables
-
-select relname as table_name, reltuples::bigint as approximate_row_count
-from pg_class
-where relname in ('datavalue', 'trackedentityinstance', 'trackedentityattributevalue', 'programstageinstance', 'trackedentitydatavalue')
-order by approximate_row_count desc;
-
 -- Approximate comparison between counts of raw data and analytics data
 
 drop view if exists _raw_count;
