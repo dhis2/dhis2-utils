de3d18e327848b005faebc01880f8db0ddb9fc6d
Davide Barbato <davide@dhis2.org>
2022-03-04 12:06:15 +0100

    Apply pep8 formatting

--- tools/dhis2-audit-data-extractor/extract_audit.py
+++ tools/dhis2-audit-data-extractor/extract_audit.py
@@ -2,93 +2,102 @@ import psycopg2
 import json
 import gzip
 import configparser
+from memory_profiler import profile
 
-DHIS2_CONF_FILE="/home/dhis/config/dhis.conf"
+DHIS2_CONF_FILE = "/home/dhis/config/dhis.conf"
 CONN_CONFIG = {
-	"host": None,
-	"dbname": None,
-	"username": None,
-	"password": None
+    "host": None,
+    "dbname": None,
+    "username": None,
+    "password": None
 }
 CUR_SIZE = 10
 
+
 def iter_row(cursor, size=CUR_SIZE):
-	while True:
-		rows = cursor.fetchmany(size)
-		if not rows:
-			break
-		for row in rows:
-			yield row
-
-with open(DHIS2_CONF_FILE, 'r') as f:
-    lines = '[conf]\n' + f.read()
-
-config = configparser.ConfigParser()
-config.read_string(lines)
-
-conn_url = config.get('conf', 'connection.url')
-split_url = conn_url.split(':')
-if len(split_url) == 0:
-	print("Error: cannot find connection URL string in {0}".format(DHIS2_CONF_FILE))
-	exit(1)
-
-db_host = split_url[2].split('/')
-if len(db_host) == 1: # in this case string is jdbc:postgresql:database_name
-	CONN_CONFIG['host'] = "localhost"
-	CONN_CONFIG['dbname'] = db_host[0]
-else: # in this case string is jdbc:postgresql://remote.host/database_name
-	CONN_CONFIG['host'] = db_host[-2]
-	CONN_CONFIG['dbname'] = db_host[-1]
-
-CONN_CONFIG['username'] = config.get('conf', 'connection.username')
-CONN_CONFIG['password'] = config.get('conf', 'connection.password')
-
-for k,v in CONN_CONFIG.items():
-	if CONN_CONFIG[k] is None:
-		print("{0} is None. Parsing error. Check {1}. Quitting".format(k, DHIS2_CONF_FILE))
-		exit(1)
-
-conn = psycopg2.connect(
-    host=CONN_CONFIG['host'],
-    database=CONN_CONFIG['dbname'],
-    user=CONN_CONFIG['username'],
-    password=CONN_CONFIG['password'])
+    while True:
+        rows = cursor.fetchmany(size)
+        if not rows:
+            break
+        for row in rows:
+            yield row
 
 
-cur = conn.cursor()
+def extract_data():
+    with open(DHIS2_CONF_FILE, 'r') as f:
+        lines = '[conf]\n' + f.read()
 
-# execute a statement
-#print('PostgreSQL database version:')
-#cur.execute('SELECT version()')
+    config = configparser.ConfigParser()
+    config.read_string(lines)
 
-# display the PostgreSQL database server version
-#db_version = cur.fetchone()
-#print(db_version)
-       
-cur.execute('SELECT * from audit')
-audit_raw_data = list()
+    conn_url = config.get('conf', 'connection.url')
+    split_url = conn_url.split(':')
+    if len(split_url) == 0:
+        print("Error: cannot find connection URL string in {0}".format(
+            DHIS2_CONF_FILE))
+        exit(1)
 
-for row in iter_row(cur):
-	audit_raw_data.append(row)
+    db_host = split_url[2].split('/')
+    if len(db_host) == 1:  # in this case string is jdbc:postgresql:database_name
+        CONN_CONFIG['host'] = "localhost"
+        CONN_CONFIG['dbname'] = db_host[0]
+    else:  # in this case string is jdbc:postgresql://remote.host/database_name
+        CONN_CONFIG['host'] = db_host[-2]
+        CONN_CONFIG['dbname'] = db_host[-1]
 
-#close the communication with the PostgreSQL
-cur.close()  
+    CONN_CONFIG['username'] = config.get('conf', 'connection.username')
+    CONN_CONFIG['password'] = config.get('conf', 'connection.password')
 
-audit_data = list()
-for data in audit_raw_data:
-	event = {
-		"id": data[0],
-		"event": data[1],
-		"type": data[2],
-		"datetime": data[3].strftime("%Y-%m-%d %H:%M:%S"),
-		"createdby": data[4],
-		"klass": data[5],
-		"uid": data[6],
-		"code": data[7],
-		"attributes": data[8],
-		"data": json.loads(gzip.decompress(data[9]).decode('utf-8'))
-	}
-	audit_data.append(event)
+    for k, v in CONN_CONFIG.items():
+        if CONN_CONFIG[k] is None:
+            print("{0} is None. Parsing error. Check {1}. Quitting".format(
+                k, DHIS2_CONF_FILE))
+            exit(1)
 
-#print(audit_data)
-print(json.dumps(audit_data))
+    conn = psycopg2.connect(
+        host=CONN_CONFIG['host'],
+        database=CONN_CONFIG['dbname'],
+        user=CONN_CONFIG['username'],
+        password=CONN_CONFIG['password'])
+
+    cur = conn.cursor()
+
+    # execute a statement
+    #print('PostgreSQL database version:')
+    #cur.execute('SELECT version()')
+
+    # display the PostgreSQL database server version
+    #db_version = cur.fetchone()
+    # print(db_version)
+
+    cur.execute('SELECT * from audit')
+    audit_raw_data = list()
+
+    for row in iter_row(cur):
+        audit_raw_data.append(row)
+
+    # close the communication with the PostgreSQL
+    cur.close()
+
+    audit_data = list()
+    for data in audit_raw_data:
+        event = {
+            "id": data[0],
+            "event": data[1],
+            "type": data[2],
+            "datetime": data[3].strftime("%Y-%m-%d %H:%M:%S"),
+            "createdby": data[4],
+            "klass": data[5],
+            "uid": data[6],
+            "code": data[7],
+            "attributes": data[8],
+            "data": json.loads(gzip.decompress(data[9]).decode('utf-8'))
+        }
+        audit_data.append(event)
+
+    # print(audit_data)
+    print(json.dumps(audit_data))
+
+
+if __name__ == '__main__':
+    extract_data()
