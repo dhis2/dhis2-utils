d90b6cdb8231da79c708da8ace81d947a6c8394e
Lars Helge Øverland <lars@dhis2.org>
2023-09-30 22:46:38 +0200

    chore: Add clean_data.sql

---
+++ resources/sql/clean_data.sql
@@ -0,0 +1,39 @@
+
+-- Script which removes all data related tables,
+-- which includes data value, tracked entity instance and program stage instance related tables
+
+-- Create function
+
+drop function if exists delete_rows_from_tables;
+
+create or replace function delete_rows_from_tables(tables_to_delete text[])
+returns void as
+$$
+declare
+  table_name text;
+begin
+  foreach table_name in array tables_to_delete
+  loop
+    execute 'delete from "' || table_name || '"';
+  end loop;
+end;
+$$
+language plpgsql;
+
+-- Remove data
+
+select delete_rows_from_tables(array[
+'datavalue',
+'programownershiphistory',
+'trackedentitydatavalueaudit',
+'trackedentityattributevalueaudit',
+'trackedentityinstanceaudit',
+'programstageinstance_messageconversation',
+'programstageinstancecomments',
+'programinstancecomments',
+'programstageinstance',
+'programinstance',
+'trackedentityattributevalue',
+'trackedentityprogramowner',
+'trackedentityinstance'
+]);
