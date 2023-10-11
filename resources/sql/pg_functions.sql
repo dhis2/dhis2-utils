d156c49f97e9cff8fed43a05ef830966ad00e1b5
Lars Helge Øverland <lars@dhis2.org>
2023-10-11 11:19:48 +0200

    feat: Add pg_functions.sql

---
+++ resources/sql/pg_functions.sql
@@ -0,0 +1,23 @@
+
+--
+-- PostgreSQL utility functions
+--
+
+-- Get approximate row count of table in a performant way
+--
+-- Usage: select approx_count('table_name')
+
+create or replace function approx_count(table_name text)
+returns bigint as
+$$
+declare
+  row_count_estimate bigint;
+begin
+  select reltuples into row_count_estimate
+  from pg_class 
+  where relname = table_name;
+  
+  return row_count_estimate;
+end;
+$$
+language plpgsql;
