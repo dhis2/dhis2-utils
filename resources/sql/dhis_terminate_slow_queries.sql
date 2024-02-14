4851e52fa90cd1582b256b42ec8a9c9a42d1f02d
Lars Helge Øverland <lars@dhis2.org>
2024-02-14 16:15:38 +0100

    fix: Update functions

--- resources/sql/dhis_terminate_slow_queries.sql
+++ resources/sql/dhis_terminate_slow_queries.sql
@@ -5,15 +5,24 @@
 
 -- To invoke run $ select dhis_terminate_slow_queries();
 
-drop function if exists dhis_terminate_slow_queries();
+-- Create view
 
-create function dhis_terminate_slow_queries()
+create or replace view dhis_slow_queries as 
+select * from pg_catalog.pg_stat_activity 
+where usename = 'dhis' 
+and application_name not in ('psql', 'pg_dump') 
+and query ilike 'select%' 
+and query !~* ('pg_catalog|information_schema|pg_temp|pg_toast');
+
+-- Create function
+
+create or replace function dhis_terminate_slow_queries()
 returns integer as $$
 declare
 	q record;
 	c integer := 0;
 begin
-	for q in select * from pg_catalog.pg_stat_activity where (now() - pg_stat_activity.query_start) > interval '2 minutes' and usename = 'dhis' and query ilike 'select%'
+	for q in select * from dhis_slow_queries
 	loop
 		raise notice 'Terminating query with PID: %', q.pid;
 		perform pg_terminate_backend(q.pid);
