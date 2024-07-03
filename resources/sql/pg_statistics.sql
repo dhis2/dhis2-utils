99784056d67a915971466683e30614af0cd45f04
Lars Helge Øverland <lars@dhis2.org>
2024-07-03 10:58:22 +0200

    fix: Add SQL statement

--- resources/sql/pg_statistics.sql
+++ resources/sql/pg_statistics.sql
@@ -166,3 +166,18 @@ where t.table_type = 'BASE TABLE'
 and c.relname !~ '^_?pg_.*$'
 order by approximate_row_count desc
 limit 200;
+
+-- Foreign keys referring to a specific table
+
+select conrelid::regclass AS foreign_table,
+  conname AS constraint_name,
+  a.attname AS foreign_column,
+  confrelid::regclass AS referenced_table,
+  af.attname AS referenced_column,
+  pg_get_constraintdef(c.oid) AS constraint_definition
+from pg_constraint c
+  inner join pg_attribute a ON a.attnum = ANY(c.conkey) AND a.attrelid = c.conrelid
+  inner join pg_attribute af ON af.attnum = ANY(c.confkey) AND af.attrelid = c.confrelid
+where confrelid = 'organisationunit'::regclass
+  and c.contype = 'f';
+
