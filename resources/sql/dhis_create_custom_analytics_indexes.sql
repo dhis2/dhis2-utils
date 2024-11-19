d364c926d286ee0e390561406d668aae9d638fd2
Lars Helge Øverland <lars@dhis2.org>
2024-11-19 16:12:14 +0100

    fix: Add file dhis_create_custom_analytics_indexes.sql

---
+++ resources/sql/dhis_create_custom_analytics_indexes.sql
@@ -0,0 +1,27 @@
+--
+-- SQL script for adding custom indexes to DHIS 2 analytics tables
+--
+-- Statements can be run with a DHIS 2 analytics table hook or with a cron job
+--
+-- Index statements should be adjusted to match frequent and slow anaytics queries
+--
+-- Cron job to run every morning after analytics tables are populated and before business hours
+--
+-- 30 6 * * *      /bin/bash -c 'psql -d dhis2 -f /var/lib/pgsql/scripts/dhis_create_custom_analytics_indexes.sql'
+--
+
+-- 2024
+create index if not exists "in_dx_uidlevel2_2024" on public.analytics_2024 using btree (dx, uidlevel1);
+create index if not exists "in_dx_uidlevel2_2024" on public.analytics_2024 using btree (dx, uidlevel2);
+create index if not exists "in_dx_uidlevel3_2024" on public.analytics_2024 using btree (dx, uidlevel3);
+create index if not exists "in_dx_uidlevel4_2024" on public.analytics_2024 using btree (dx, uidlevel4);
+create index if not exists "in_dx_monthly_2024" on public.analytics_2024 using btree (dx, monthly);
+create index if not exists "in_dx_quarterly_2024" on public.analytics_2024 using btree (dx, quarterly);
+
+-- 2023
+create index if not exists "in_dx_uidlevel2_2024" on public.analytics_2023 using btree (dx, uidlevel1);
+create index if not exists "in_dx_uidlevel2_2023" on public.analytics_2023 using btree (dx, uidlevel2);
+create index if not exists "in_dx_uidlevel3_2023" on public.analytics_2023 using btree (dx, uidlevel3);
+create index if not exists "in_dx_uidlevel4_2023" on public.analytics_2023 using btree (dx, uidlevel4);
+create index if not exists "in_dx_monthly_2023" on public.analytics_2023 using btree (dx, monthly);
+create index if not exists "in_dx_quarterly_2023" on public.analytics_2023 using btree (dx, quarterly);
